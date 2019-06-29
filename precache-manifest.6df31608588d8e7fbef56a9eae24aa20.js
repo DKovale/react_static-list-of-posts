810c7fb520ae04e3244bba77c1e36f89bffc2999
DKovale <dkov@ukr.net>
2019-06-29 20:13:31 +0300

    Updates

---
+++ precache-manifest.6df31608588d8e7fbef56a9eae24aa20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "061c22482207c80d981c2173ef366852",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "9c8db2e2331b39f44fb3",
+    "url": "/react_static-list-of-posts/static/css/main.dcfbdd64.chunk.css"
+  },
+  {
+    "revision": "0d93b29b27d1d199691f",
+    "url": "/react_static-list-of-posts/static/js/2.95678282.chunk.js"
+  },
+  {
+    "revision": "9c8db2e2331b39f44fb3",
+    "url": "/react_static-list-of-posts/static/js/main.010b4fce.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
